e616e08f74372930594720af9094adfea0d58704
umoruj <umoru80@gmail.com>
2016-03-08 06:33:15 +0100

    including signal strength in data sent

--- models/location.js
+++ models/location.js
@@ -5,7 +5,12 @@ var mongoose = restful.mongoose;
 var locationSchema = new mongoose.Schema({
 	userID: String,
 	location: String,
-	timestamp: String
+	timestamp: String,
+	signalStrength1: String,
+	signalStrength2: String,
+	signalStrength3: String,
+	signalStrength4: String
+
 });
 
 module.exports = restful.model('location', locationSchema);
