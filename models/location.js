02da87c7e24833571ea6f934abaa6c39a02bec93
umoruj <umoru80@gmail.com>
2016-03-21 14:54:30 +0100

    change from signal strenth to proximity distance

--- models/location.js
+++ models/location.js
@@ -5,11 +5,11 @@ var mongoose = restful.mongoose;
 var locationSchema = new mongoose.Schema({
 	userID: String,
 	location: String,
-	timestamp: String,
-	signalStrength1: String,
-	signalStrength2: String,
-	signalStrength3: String,
-	signalStrength4: String
+	timestamp: Number,
+	distance1: Number,
+	distance2: Number,
+	distance3: Number,
+	distanec4: Number
 
 });
 
